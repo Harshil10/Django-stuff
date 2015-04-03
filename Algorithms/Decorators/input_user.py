8561417498d9d9fe6770b3fef76f348cc328e8fc
Harshil10 <harshil492@yahoo.com>
2015-04-03 22:50:39 +0530

    Modified Decorator to take arguments

--- Algorithms/Decorators/input_user.py
+++ Algorithms/Decorators/input_user.py
@@ -1,4 +1,5 @@
 import sys, os
+from functools import wraps
 
 '''
 @author = Harshil Modi
@@ -7,12 +8,20 @@ import sys, os
 '''
 Decorator function to take inputs from User
 '''
-def decorate_input(func):
+def list_input():
     arr = []
-    def inner():
-        elem_count = raw_input('Enter Number of elements to Sort: ')
-        for i in xrange(0, int(elem_count)):
-            elem = raw_input()
-            arr.append(int(elem))
-        func(arr)
+    elem_count = raw_input('Enter Number of elements to Sort: ')
+    for i in xrange(0, int(elem_count)):
+        elem = raw_input()
+        arr.append(int(elem))
+    return arr
+
+def decorate_input(argument):
+    def inner(func):
+        @wraps(func)
+        def inner_most():
+            if argument == 'list':
+                array = list_input()
+                func(array)
+        return inner_most
     return inner
